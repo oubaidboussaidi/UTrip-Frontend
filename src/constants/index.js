2b431ba581eea3862b0aa39499cb5c861b1904f2
oubaidboussaidi <oubaydboussaidi@gmail.com>
2025-11-28 15:30:00 +0100

    Add Constants

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+const navLinks = [
+  { 
+    name: "Home", 
+    link: "#hero" 
+  },
+  { name: "Events",
+    link: "#events"
+  },
+  { name: "Tours", 
+    link: "#tours" 
+  },
+  { name: "About Us", 
+    link: "#about" 
+  },
+  
+];
+
+
+
+export {
+  navLinks,
+};
